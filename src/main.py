580af77f6eb1e014405f0201cb3dc99b45bb7730
Collin Hockey <cjhockey@gmail.com>
2012-03-15 21:25:43 -0400

    New features to the maze class
    - Now finds the start location and keeps track of it
    - Made __call__ function for getting the cell type at any spot in the maze

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
+#!/usr/bin/python
+
 import mazelib
 
 if __name__ == "__main__":
     maze = mazelib.Maze("../mazes/maze1.txt")
 
     print maze
+    print ("Start: (" + str(maze.start_row) + "," +
+           str(maze.start_col) + ") : " +
+           str(maze(maze.start_row, maze.start_col)))
--- src/mazelib/maze.py
+++ src/mazelib/maze.py
@@ -7,11 +7,24 @@ class Maze:
 
         self._maze = []
 
+        rownum = 0
         for line in maze_file:
             this_row = []
+            colnum = 0
             for cell in line.strip():
-                this_row.append(cells.cell_factory(cell))
+                maze_cell = cells.cell_factory(cell)
+                if(maze_cell.is_start()):
+                    self.start_col = colnum
+                    self.start_row = rownum
+
+                this_row.append(maze_cell)
+                colnum = colnum + 1
+                
             self._maze.append(this_row)
+            rownum = rownum + 1
+
+    def __call__(self, row, col):
+        return self._maze[row][col]
         
     def __str__(self):
         return "\n".join(["".join([str(cell) for cell in row]) 
