f40f7457d9619c3196c495d249504e96b2e7df65
Collin Hockey <cjhockey@gmail.com>
2012-04-22 16:02:54 -0400

    added a graphic bug, which can paint itself and figure out where it should go

---
+++ src/mazelib/graphicbug.py
@@ -0,0 +1,47 @@
+import pygame
+import pygame.locals as pg
+
+from bug import Bug
+
+TILE_WIDTH, TILE_HEIGHT = 32, 32
+
+class GraphicBug(Bug):
+
+    def __init__(self, maze):
+        super(GraphicBug, self).__init__(maze)
+        self._rotate_angles = {
+            Bug.NORTH : 0,
+            Bug.EAST  : 270,
+            Bug.SOUTH : 180,
+            Bug.WEST  : 90
+        }
+
+    def render(self):
+        """
+        Create a tile-sized triangle with the correct facing for a bug
+        """
+        image = pygame.Surface((TILE_WIDTH, TILE_HEIGHT))
+        transparent_color = (255, 0, 255)
+        image.set_colorkey(transparent_color)
+        image.fill(transparent_color)
+
+        points = [(TILE_WIDTH / 2, 0), 
+                  (0, TILE_HEIGHT), 
+                  (TILE_WIDTH, TILE_HEIGHT)]
+
+        blue_color = (0, 0, 255)
+        pygame.draw.polygon(image, blue_color, points)
+        
+        rotate_angle = self._rotate_angles[self.get_facing()]
+        print self.get_facing()
+
+        image = pygame.transform.rotate(image, rotate_angle)
+
+        return image
+
+    def get_render_location(self):
+        """
+        Get the screen location the bug should be blitted to
+        """
+        (row, col) = self.get_location()
+        return (TILE_HEIGHT * col, TILE_WIDTH * row)
